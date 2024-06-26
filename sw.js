b3aca9369b8b7d72bce42142ea8135f0791b6292
MashiroCode <58067436+MashiroCode@users.noreply.github.com>
2024-06-27 07:07:09 +0800

    Site updated: 2024-06-27 07:07:09

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1718931752513";
+const VERSION = "1719443223338";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
